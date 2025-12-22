08d84e2fadf0bbaf66f266d63f591b0697badbd4
Justin Tweed <114602217+WarGuy17@users.noreply.github.com>
2025-12-22 13:26:30 -0500

    Beginning of project.
    Added html fully. Almost done with CSS only a couple more ideas. Javascript needs a lot more work, only starting to connect it to the html.

---
+++ script.js
@@ -0,0 +1,12 @@
+let header = document.querySelectorAll('.header');
+let main = document.querySelectorAll('#main');
+let paragraph = document.querySelectorAll('#paragraph');
+let form = document. querySelectorAll('#form');
+let input = document.querySelectorAll('.input');
+let button = document.querySelectorAll('.button');
+let pokemonPicture = document.querySelectorAll('#pokemon_picture');
+let pokemonData = document.querySelectorAll('#pokemon_data');
+
+input.addEventListener(input, function( {
+    console.log('hello'));
+})
